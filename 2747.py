0a84b2c02101294ac0e9b6c0f7120be9e2228d70
Leo <43527720+lefredoh@users.noreply.github.com>
2023-02-17 18:29:20 -0300

    Add files via upload

---
+++ 2747.py
@@ -0,0 +1,31 @@
+'''
+Salida 1
+A tu profesor de programación le gustaría hacer una pantalla con las siguientes características:
+   Tener 39 guiones (-) en la primera línea;
+   Tener un | debajo del primer guión y el trigésimo noveno guión de la primera línea, rellene el medio con espacios en blanco;
+   Repita el procedimiento 2 más cuatro veces;
+   Repita el procedimiento 1.
+
+Al final debe verse como la siguiente imagen:
+
+--------------------------------------- (39 traces)
+|                                     |
+|                                     |
+|                                     |
+|                                     |
+|                                     |
+--------------------------------------- (39 traces)
+Input
+No hay.
+
+Output
+La salida se imprimirá como se muestra arriba.
+'''
+
+print('-'*39)
+print('|'+' '*37+'|')
+print('|'+' '*37+'|')
+print('|'+' '*37+'|')
+print('|'+' '*37+'|')
+print('|'+' '*37+'|')
+print('-'*39)
