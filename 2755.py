88270fdf63f29da1d04e38ff32644ad8c87cac82
Leo <43527720+lefredoh@users.noreply.github.com>
2023-02-21 15:54:33 -0300

    Add files via upload

---
+++ 2755.py
@@ -0,0 +1,18 @@
+'''
+Su profesor le gustaría que usted hiciera un programa con las siguientes características:
+
+   Muestre la siguiente frase en la pantalla: "Ro'b'er to\ /" (Entre el r y el t tiene una tabulacion);
+   Muestre la siguiente frase en la pantalla: (._.) ( l: ) ( .-. ) ( :l ) (._.);
+   Muestre la siguiente frase en la pantalla: (^_-) (-_-) (-_^);
+   Muestre la siguiente frase en la pantalla: ("_") ('.');
+
+Entrada
+No hay.
+
+Salida
+El resultado de su programa se debe escribir según el ejemplo de salida.
+'''
+print('''"Ro'b'er\tto\/"
+(._.) ( l: ) ( .-. ) ( :l ) (._.)
+(^_-) (-_-) (-_^)
+("_") ('.')''')
