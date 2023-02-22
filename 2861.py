684aec49a256332e008e7a809a5872e6e4b9ebb0
Leo <43527720+lefredoh@users.noreply.github.com>
2023-02-22 17:14:40 -0300

    Add files via upload

---
+++ 2861.py
@@ -0,0 +1,19 @@
+'''
+Cacunda, Bizz y Massacote son amigos inseparables. En la universidad, en unos días, no fueron a clase a jugar una mala pasada. Un día pasaba un profesor. Al mismo tiempo, los tres gritaron la palabra "gzuz" en voz alta. Después de este grito, se volvieron invisibles y el maestro no los vio. Nuevamente, su clase estaba respondiendo preguntas del maestro. Cuando era el turno de uno de ellos, respondía con la palabra "gzuz", y el profesor aceptaba la respuesta y daba la nota máxima de la pregunta. Hacer la simulación de la salida que encontraron para salir de los más diversos problemas.
+
+Input
+La entrada se compone de varios casos de prueba. La primera línea contiene un número entero C (2 <= C <= 99) relativo al número de preguntas que ha hecho el profesor. Las siguientes líneas C vienen con una pregunta hecha por el maestro.
+
+Output
+Para cada pregunta, escriba la respuesta que dijeron los tres amigos.'''
+
+
+n = int(input())
+
+lista = []
+for i in range(n):
+   l = input()
+   lista.append('gzuz')
+
+for i in lista:
+   print(i)
